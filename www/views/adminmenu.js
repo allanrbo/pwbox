6b977218735cd2e3846ac1bfedba7a043f7d63e9
Allan Boll <allan@acoby.com>
2018-03-02 19:45:54 -0800

    Backup menu

--- www/views/adminmenu.js
+++ www/views/adminmenu.js
@@ -10,6 +10,7 @@ var AdminMenu = {
             m("p", m("a[href=/admin/profile]", {oncreate: m.route.link}, "My user profile")),
             isAdmin ? m("p", m("a[href=/admin/users]", {oncreate: m.route.link}, "Users")) : null,
             isAdmin ? m("p", m("a[href=/admin/groups]", {oncreate: m.route.link}, "User groups")) : null,
+            isAdmin ? m("p", m("a[href=/admin/backuprestore]", {oncreate: m.route.link}, "Backup and restore")) : null,
         ];
     }
 }
