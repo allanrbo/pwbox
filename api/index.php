f85889ae23d939cdbbd0b8b61c88b62a63b52adf
Allan Boll <allan@acoby.com>
2018-03-13 02:02:57 -0700

    Added ensureSystemUserExists to index.php

--- api/index.php
+++ api/index.php
@@ -13,6 +13,8 @@ header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
 
+ensureSystemUserExists();
+
 $method = $_SERVER["REQUEST_METHOD"];
 $uriPrefix = getconfig()["uriPrefix"];
 $uri = $_SERVER["REQUEST_URI"];
