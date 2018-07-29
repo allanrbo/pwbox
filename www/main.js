c44237cbd15e69354cc0a088bc1b6aa419dc4b1a
Allan Boll <allan@acoby.com>
2018-07-28 19:36:16 -0700

    Fixed unauthorized warning before login

--- www/main.js
+++ www/main.js
@@ -1,13 +1,16 @@
-m.route(document.body, "/secrets", {
-    "/login": { render: function() { return m(LoginForm); } },
-    "/secrets": { render: function() { return m(Layout, m(SecretList)); } },
-    "/secrets/:key": { render: function(vnode) { return m(Layout, vnode.attrs, m(SecretForm, vnode.attrs)); } },
-    "/admin": { render: function() { return m(Layout, m(AdminMenu)); } },
-    "/admin/groups": { render: function() { return m(Layout, m(GroupList)); } },
-    "/admin/groups/:key": { render: function(vnode) { return m(Layout, vnode.attrs, m(GroupForm, vnode.attrs)); } },
-    "/admin/users": { render: function() { return m(Layout, m(UserList)); } },
-    "/admin/users/:key": { render: function(vnode) { return m(Layout, vnode.attrs, m(UserForm, vnode.attrs)); } },
-    "/admin/profile": { render: function() { return m(Layout, m(Profile)); } },
-    "/admin/backuprestore": { render: function() { return m(Layout, m(BackupRestorePage)); } },
-    "/logout": { render: function() { Session.logout(); m.route.set("/login"); } }
-});
+m.route(
+    document.body,
+    "/login", {
+        "/login": { render: function() { return m(LoginForm); } },
+        "/secrets": { render: function() { return m(Layout, m(SecretList)); } },
+        "/secrets/:key": { render: function(vnode) { return m(Layout, vnode.attrs, m(SecretForm, vnode.attrs)); } },
+        "/admin": { render: function() { return m(Layout, m(AdminMenu)); } },
+        "/admin/groups": { render: function() { return m(Layout, m(GroupList)); } },
+        "/admin/groups/:key": { render: function(vnode) { return m(Layout, vnode.attrs, m(GroupForm, vnode.attrs)); } },
+        "/admin/users": { render: function() { return m(Layout, m(UserList)); } },
+        "/admin/users/:key": { render: function(vnode) { return m(Layout, vnode.attrs, m(UserForm, vnode.attrs)); } },
+        "/admin/profile": { render: function() { return m(Layout, m(Profile)); } },
+        "/admin/backuprestore": { render: function() { return m(Layout, m(BackupRestorePage)); } },
+        "/logout": { render: function() { Session.logout(); m.route.set("/login"); } }
+    }
+);
